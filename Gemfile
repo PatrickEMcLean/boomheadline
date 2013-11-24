96f305d7e08ef9f9009598ff8e9a6dd2e2dd56fd
Scott Helm <helm.scott@gmail.com>
2013-11-24 16:31:42 -0500

    cleans up and updates gemfile to use git version (latest) for stripe

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
+gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
 gem 'sinatra', "~> 1.4.4"
-gem "stripe", "~> 1.8.8"
 gem 'haml', "~> 4.0.4"
 gem 'freshdesk'
--- boom.rb
+++ boom.rb
@@ -9,11 +9,11 @@ enable :logging
 set :publishable_key, "pk_test_CFehYFpcPnoGuWeZwA6TqrWS"
 set :secret_key, "sk_test_8CTp0hYKpOziGUCBSqQKRdux"
 
-
 Stripe.api_key = settings.secret_key
 
-get '/' do 
-	haml :form, :layout => :main
+
+get '/' do
+  haml :form, :layout => :main
 end
 
 
@@ -21,55 +21,59 @@ end
 post '/promo' do
 
 
-	
-	if params[:promo]
-		if params[:promocode] == "22"
-			client.post_tickets(:email => params[:email], :description => ticket, :name => "Customer Name", :source => 2, :priority => 2, :name => "Joshua Siler")
-			haml :response, :layout => :main
-		else
-			haml :badcode, :layout => :main
-		end
-	end
+
+  if params[:promo]
+    if params[:promocode] == "22"
+      client.post_tickets(:email => params[:email], :description => ticket, :name => "Customer Name", :source => 2, :priority => 2, :name => "Joshua Siler")
+      haml :response, :layout => :main
+    else
+      haml :badcode, :layout => :main
+    end
+  end
 end
 
 post '/charge' do
-	
-
-	ticket = "\n\n\n"
-	ticket += "Their line: #{params[:line]}"
-	ticket += "\n\n\n\n"
-	ticket += "Wants them to: #{params[:what]}"
-	ticket += "\n\n\n"
-	ticket += "Is writing for: #{params[:who]}"
-	ticket += "\n\n\n"
-	ticket += "With this kind of tone: #{params[:tone]}"
-	ticket += "\n\n\n"
-	ticket += "Wants them to: #{params[:what]}"
-	ticket += "\n\n\n"
-	ticket += "And consider this: #{params[:whatelse]}"
-	ticket += "\n\n\n"
-
-	client = Freshdesk.new("http://boomheadline.freshdesk.com/", "patrickemclean@gmail.com", "boompassword") 
 
 
-	client.post_tickets(:email => params[:email], :description => ticket, :name => params[:name], :source => 2, :priority => 2, :name => "Joshua Siler")
+  ticket = "\n\n\n"
+  ticket += "Their line: #{params[:line]}"
+  ticket += "\n\n\n\n"
+  ticket += "Wants them to: #{params[:what]}"
+  ticket += "\n\n\n"
+  ticket += "Is writing for: #{params[:who]}"
+  ticket += "\n\n\n"
+  ticket += "With this kind of tone: #{params[:tone]}"
+  ticket += "\n\n\n"
+  ticket += "Wants them to: #{params[:what]}"
+  ticket += "\n\n\n"
+  ticket += "And consider this: #{params[:whatelse]}"
+  ticket += "\n\n\n"
 
-	logger.info "params, bitches"
-	logger.info params
-	logger.info "client.inspect"
-	logger.info client.inspect
-	logger.info "Stripe api key"
-	logger.info Stripe.api_key
-	begin
-	charge = Stripe::Charge.create(:amount => 2000, :currency => "usd", :card => params[:stripeToken], :description => "Boom! Headline.")
-	rescue Exception => e
-	  logger.info e
-	end
-	logger.info charge.inspect
-
-
-	haml :response, :layout => :main
-	
+  client = Freshdesk.new(
+    "http://boomheadline.freshdesk.com/",
+    "patrickemclean@gmail.com",
+    "boompassword"
+  )
+  client.post_tickets(
+    :email => params[:email],
+    :description => ticket,
+    :name => params[:name],
+    :source => 2,
+    :priority => 2,
+    :name => "Joshua Siler"
+  )
+  begin
+    charge = Stripe::Charge.create(
+      :amount => 2000,
+      :currency => "usd",
+      :card => params[:stripeToken],
+      :description => "Boom! Headline."
+    )
+  rescue Exception => e
+    logger.info e
+    haml :badcode
+  end
+  haml :response, :layout => :main
 end
 
 error Stripe::CardError do
