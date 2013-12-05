a6b45de28ad5251d0eef0409509a26076cc7bbe0
PatrickEMcLean <patrickemclean@gmail.com>
2013-12-04 21:56:46 -0500

    more elegant blog solution

--- boom.rb
+++ boom.rb
@@ -11,36 +11,45 @@ enable :logging
 set :stripe_publishable_key, "pk_test_CFehYFpcPnoGuWeZwA6TqrWS"
 set :stripe_secret_key, "sk_test_3UqpOIA4twxvA33Y1VQmLjOq"
 
-def jekyll_blog(path, &missing_file_block)
-          # @current_menu = "blog"
-          # @title = "Blog - Derek Eder"
+get '/blog' do
+  send_file File.join(File.dirname(__FILE__), '/public/blog/_site/index.html')
+end
+
+get '/blog/:title' do
+  post = File.basename(request.path)
+  send_file File.join(File.dirname(__FILE__), "/public/blog/_site/blog/#{post}/index.html")
+end
+
+# def jekyll_blog(path, &missing_file_block)
+#           # @current_menu = "blog"
+#           # @title = "Blog - Derek Eder"
         
-          file_path = File.join(File.dirname(__FILE__), 'public/blog/_site',  path.gsub('/blog',''))
-          logger.info file_path
-          file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i  
-          logger.info file_path
+#           file_path = File.join(File.dirname(__FILE__), 'public/blog/_site',  path.gsub('/blog',''))
+#           logger.info file_path
+#           file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i  
+#           logger.info file_path
 
-          logger.info File.exist?(file_path)
+#           logger.info File.exist?(file_path)
 
-          if File.exist?(file_path)
-            file = File.open(file_path, "rb")
-            contents = file.read
-            file.close
+#           if File.exist?(file_path)
+#             file = File.open(file_path, "rb")
+#             contents = file.read
+#             file.close
         
-            if (file_path.include?('.xml') || file_path.include?('.css'))
-              erb contents, :content_type => 'text/xml'
-            else
-              erb contents, :layout_engine => :haml
-            end
-          else
-            haml :not_found
-          end
-        end
+#             if (file_path.include?('.xml') || file_path.include?('.css'))
+#               erb contents, :content_type => 'text/xml'
+#             else
+#               erb contents, :layout_engine => :haml
+#             end
+#           else
+#             haml :not_found
+#           end
+#         end
 
- get "/blog/?*" do
-              logger.info request.path
-              jekyll_blog(request.path) {404}
-            end
+#  get "/blog/?*" do
+#               logger.info request.path
+#               jekyll_blog(request.path) {404}
+#             end
 
 get '/' do
   haml :home, :layout => :main
